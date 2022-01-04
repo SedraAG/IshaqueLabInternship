aa5acb941e4d8e9b81644751ef6a2627a453439e
SedraAG <sedra.aboughaloun@fu-berlin.de>
2022-01-04 18:36:22 +0100

    new plots

--- Module/cnv.py
+++ Module/cnv.py
@@ -1,48 +1,52 @@
 import numpy as np
 import pandas as pd
 from Bio import SeqIO
-import seaborn as sns
 from matplotlib import pyplot as plt
-import seaborn as sns
-from sklearn.cluster import DBSCAN
-from sklearn.preprocessing import StandardScaler
 from Bio.SeqUtils import GC
 plt.style.use('ggplot')
+import sys
 
-# calculate the index of the beginning of each chr in df
-# df has to have columns named chr_ind and chr 
+# calculate the end index of each chr in df
+# requires 2 different columns: chr_ind to group the df by it and any other column 
 
-def index_of_chr(df):
-    indices = df.groupby(["chr_ind"]).count()['chr']
+def add_column(df,name):
+    df[name] = np.nan
+
+
+def index_of_chr(df,chr_ind,chr):
+    indices = df.groupby([chr_ind]).count()[chr]
     my_list = []
     for i in range (1,len(indices)+1):
         if i == 1:
-            my_list.append(indices[1]-1)
+            my_list.append(indices[i]-1)
         else:
             indices[i] = indices[i] + indices[i-1]
-            my_list.append(indices[i] -1)    
+            my_list.append(indices[i] - 1)    
     return my_list
     
- 
 
-# +1 to avoid dividing by zero
+
 # s is the start of the bin and e is the end of the bin and a is the sequence
 # of the chromosome wehre the bin is located
 
-
-def countGC(s,e,a):
-    content = a[s:e]
+def countGC(start,end,sequence):
+    """ calculate the GC-cotent  of a sequnce
+    
+    ignores N-bases in calculations"""
+    content = sequence[start:end]
     na = content.count("A")
     nt = content.count("T")
     ng = content.count("G")
     nc = content.count("C")
     nat = na + nt
     ncg = ng + nc
-    return (ncg/((ncg+nat)+1))
+    return (ncg/((ncg+nat)+1)) # +1 to avoid dividing by zero
 
-
-def countGC1(s,e,a):
-    content = a[s:e]
+def countGC1(start,end,sequence):
+    """ calculate the GC-cotent  of a sequnce
+    
+    includes N-bases in calculations"""
+    content = sequence[start:end]
     return GC(content)
 
 # the difference between countGC and countGC1 is that countGC does not include the NNN..
@@ -50,10 +54,12 @@ def countGC1(s,e,a):
 # includes the N bease in the calculations. (G+C)/(A+G+C+T+N)calculate GC_content of
 # each bin in each chromosom and add them all together in one list
 
-
 def countCGall(chr_list,indices,df):
+    """ calculate the GC-cotent  of a sequnce
+    
+    ignores N-bases in calculations"""
     chr_cg = []
-    for i in range (22):
+    for i in range (22):  # for each chr
         chromosom = chr_list[i]
         index = indices[i] + 1         
         if i == 0 :
@@ -67,6 +73,9 @@ def countCGall(chr_list,indices,df):
 
 
 def countCGall1(chr_list,indices,df):
+    """ calculate the GC-cotent  of a sequnce
+    
+    includes N-bases in calculations"""
     chr_cg = []
     for i in range (22):
         chromosom = chr_list[i]
@@ -81,47 +90,30 @@ def countCGall1(chr_list,indices,df):
     return chr_cg
 
 
-# make a function for plots
-
-##
-##
-##  
-
-# now we want to nomalise the counts to the mean, median and mode
-# -> we have more than one mode, so we can take the mean of those
-# we color different chromosomes with different colors
-
-
-def normalise_to_fixed_value(counts,fixed_value):
+def normalise_to_fixed_value(df,counts,fixed_value,type):
     if fixed_value == "mean":
         mean_normalised_counts = counts * (2 / counts.mean())
-        return mean_normalised_counts
+        df["mean_normalised" + type +"_counts"] = mean_normalised_counts
     elif fixed_value == "median":
         median_normalised_counts = counts * (2 / counts.median())
-        return median_normalised_counts
+        df["median_normalised" + type +"_counts"] = median_normalised_counts
     elif fixed_value == "mode":
         mode_normalised_counts = counts * (2/counts.mode().mean())
-        return mode_normalised_counts
-    else: return
+        df["mode_normalised" + type +"_counts"] = mode_normalised_counts
 
-
-
-
-def estimate_copy_number(df):
-    user_input = input("mean, median or mode? ")
-    if (user_input == 'mean'): 
+# requires colums 'chr_ind', mean_normalised_counts, mean_normalised_counts, mode_normalised_counts
+def estimate_copy_number(df,fixed_value):
+    if (fixed_value == 'mean'): 
         copy_nr_mean = ((df.groupby(['chr_ind']).mean()).mean_normalised_counts).round(decimals = 0)
         return copy_nr_mean
-    elif (user_input == 'median'):
+    elif (fixed_value == 'median'):
         copy_nr_median=((df.groupby(['chr_ind']).median()).median_normalised_counts).round(decimals = 0)
         return copy_nr_median
-    elif (user_input == 'mode'): 
+    elif (fixed_value == 'mode'): 
         copy_nr_median = ((df.groupby(['chr_ind']).median()).mode_normalised_counts).round(decimals = 0)
         return copy_nr_median
-    else: print('please try again! ')
 
 # GC-content normalisation:
-
 # for the correlation curve:
 # we cut the big interval into 360 samller intervall (i variabel in the for  loop) 
 # and calculate the GC-conten median in each interval and add the value to the list 
@@ -131,15 +123,19 @@ def estimate_copy_number(df):
 
 
 
-## plot function
-def smoothplot(df,rs,re,steps):
+# rs: range start
+# re: range end
+# steps: ~how many points 
+# pl
+def smoothplot(df,rs,re,window_size):
     median_counts = []
     cg_window = []
-    for i in range (rs,re,steps):
+    for i in range (rs,re,window_size):
         median_counts.append(df.loc[(df.GC_content >= (i/10000)-0.01) &
                                      (df.GC_content <= (i/10000)+0.015)].counts.median())
         cg_window.append(df.loc[(df.GC_content >= (i/10000)-0.01) &
                                      (df.GC_content <= (i/10000)+0.015)].GC_content.median())
+    return median_counts, cg_window
 
 # normalization for GC content:
 # the function gc_correction normalises the read counts to the GC-content and has the 
@@ -155,14 +151,42 @@ def smoothplot(df,rs,re,steps):
 # of 0.0005 = decimal_step
 # window_size: the size of the window from where we calculate the median
 
-
-def gc_correction(df,GC_content,counts,start,end,step,decimal_place,window_size):
+def gc_ncorrection(df,GC_content,start,end,step,decimal_place,window_size):
     window_size = window_size / 2
     gc_normlised_counts = np.zeros(len(df))
     decimal_step = step / decimal_place
-    df['gc_normlised_counts'] = gc_normlised_counts
+    df['gc_normalised_normal_counts'] = gc_normlised_counts
     for i in range (start,end,step):
-        df.loc[((GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))),'gc_normlised_counts'] = df.loc[(GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))].counts / (df.loc[(GC_content >= ((i/decimal_place) - window_size)) & (GC_content < ((i/decimal_place) + decimal_step + window_size))].counts.median())*2    
+        df.loc[((GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))),'gc_normalised_normal_counts'] = df.loc[(GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))].normal_counts / (df.loc[(GC_content >= ((i/decimal_place) - window_size)) & (GC_content < ((i/decimal_place) + decimal_step + window_size))].normal_counts.median())*2   
 
-# if __name__== "__main__" :
+def gc_correction(df,GC_content,start,end,step,decimal_place,window_size):
+    window_size = window_size / 2
+    gc_normlised_counts = np.zeros(len(df))
+    decimal_step = step / decimal_place
+    df['gc_normlised_mixed_counts'] = gc_normlised_counts
+    for i in range (start,end,step):
+        df.loc[((GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))),'gc_normlised_mixed_counts'] = df.loc[(GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))].counts / (df.loc[(GC_content >= ((i/decimal_place) - window_size)) & (GC_content < ((i/decimal_place) + decimal_step + window_size))].counts.median())*2   
+
+def comma_sep_data_processing(path):
+    return(pd.read_csv(path))
+
+
+def space_sep_data_processing(path):
+    return(pd.read_csv(path,sep = '\s+'))
+
+
+
+if __name__== "__main__" :
+    bin_df = space_sep_data_processing(sys.argv[1])
+    counts_df = (comma_sep_data_processing(sys.argv[2]))
+    df = pd.merge(bin_df, counts_df, on = 'bin_ind', how = 'inner')
+    function = sys.argv[3]
+    fixed_value = sys.argv[4]
+
+    if (function == 'estimate_copy_number'):
+        normalise_to_fixed_value(df, df['counts'] , fixed_value)  
+        print(estimate_copy_number(df,fixed_value))
+        
+    
+
 
