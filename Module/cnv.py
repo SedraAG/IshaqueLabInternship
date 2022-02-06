1f7321d28e3d9c469d7bd166c2cbd410d9cb897c
SedraAG <sedra.aboughaloun@fu-berlin.de>
2022-02-06 21:54:16 +0100

    user interface

--- Module/cnv.py
+++ Module/cnv.py
@@ -1,7 +1,9 @@
+from ast import arg
+from curses import window
 import numpy as np
 import pandas as pd
+import os
 from Bio import SeqIO
-import seaborn as sns
 from matplotlib import pyplot as plt
 import seaborn as sns
 from Bio.SeqUtils import GC
@@ -9,6 +11,7 @@ plt.style.use('ggplot')
 from scipy.interpolate import interp1d
 import statsmodels.api as sm
 import argparse
+
 
 # calculate the end index of each chr in df
 # requires 2 different columns: chr_ind to group the df by it and any other column 
@@ -59,12 +62,12 @@ def countGC1(start,end,sequence):
 # includes the N bease in the calculations. (G+C)/(A+G+C+T+N)calculate GC_content of
 # each bin in each chromosom and add them all together in one list
 
-def countCGall(chr_list,indices,df):
+def countCGall(chr_list,indices,df,number_of_chr):
     """ calculate the GC-cotent  of a sequnce
     
     ignores N-bases in calculations"""
     chr_cg = []
-    for i in range (22):  # for each chr
+    for i in range (number_of_chr):  # for each chr
         chromosom = chr_list[i]
         index = indices[i]       
         if i == 0 :
@@ -170,9 +173,9 @@ def gc_ccorrection(df,GC_content,start,end,step,decimal_place,window_size):
     window_size = window_size / 2
     gc_normlised_counts = np.zeros(len(df))
     decimal_step = step / decimal_place
-    df['gc_normlised_cancer_counts'] = gc_normlised_counts
+    df['gc_normalised_cancer_counts'] = gc_normlised_counts
     for i in range (start,end,step):
-        df.loc[((GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))),'gc_normlised_cancer_counts'] = df.loc[(GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))].cancer_counts / (df.loc[(GC_content >= ((i/decimal_place) - window_size)) & (GC_content < ((i/decimal_place) + decimal_step + window_size))].cancer_counts.median())* 1804.3
+        df.loc[((GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))),'gc_normalised_cancer_counts'] = df.loc[(GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))].cancer_counts / (df.loc[(GC_content >= ((i/decimal_place) - window_size)) & (GC_content < ((i/decimal_place) + decimal_step + window_size))].cancer_counts.median())* 1804.3
 
 def gc_correction(df,GC_content,start,end,step,decimal_place,window_size):
     window_size = window_size / 2
@@ -182,54 +185,250 @@ def gc_correction(df,GC_content,start,end,step,decimal_place,window_size):
     for i in range (start,end,step):
         df.loc[((GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))),'gc_normlised_mixed_counts'] = df.loc[(GC_content >= (i/decimal_place)) & (GC_content < ((i/decimal_place) + decimal_step))].mixed_counts / (df.loc[(GC_content >= ((i/decimal_place) - window_size)) & (GC_content < ((i/decimal_place) + decimal_step + window_size))].mixed_counts.median())*2   
 
-
-
 def gc_correction_withh_lowess(df, counts, frac):
-    lowess=sm.nonparametric.lowess(df[counts],df['GC_content'],frac=frac)
-    return (df[counts]/list(zip(*lowess))[1])*2
-
+    lowess = sm.nonparametric.lowess(df[counts],df['GC_content'],frac=frac)
+    copy = df.sort_values("GC_content", ignore_index = True)
+    copy['lowess_gc_corrected_cancer_counts'] = copy[counts] / (list(zip(*lowess))[1])
+    copy = copy.sort_values("bin_ind", ignore_index = True)
+    return copy['lowess_gc_corrected_cancer_counts'] 
     
 def comma_sep_data_processing(path):
     return(pd.read_csv(path))
 
-
 def space_sep_data_processing(path):
     return(pd.read_csv(path,sep = '\s+'))
 
-def simple_scatter_plot(x,y,x_label,y_label,titel):
+def simple_scatter_plot(df,x,y,x_label,y_label,titel): 
+    plt.figure(figsize=(16, 4))
     plt.scatter(x, y, s = 25)
+    if (x is df["bin_ind"]):
+        indices = index_of_chr(df, "chr_ind", "bin_start")
+        del indices[-1:]
+        for i in indices:
+            plt.axvline(x=df.bin_ind[i], ymin=0, ymax=1, c='black')
     plt.title(titel, fontdict=None,fontsize=30, loc='center', pad=None)
     plt.xlabel(x_label, size = 25)
     plt.ylabel(y_label, size = 25)
-    plt.show()
+    plt.savefig(titel+'.png', dpi=300, bbox_inches='tight')
+    plt.close()
 
 
+params = {'legend.fontsize': 'x-large',
+          'figure.titlesize': 20,
+          "figure.autolayout": True,
+         'scatter.marker': '.',
+         'scatter.edgecolors': 'black'}
+plt.rcParams.update(params)
+    
 
 
 def main():
-    parser  = argparse.ArgumentParser()
-    parser.add_argument('-H','--normal_counts_file',metavar='', type = argparse.FileType('r'), help = "file path to counts df")
-    parser.add_argument('-C','--cancer_counts_file',metavar='', type = argparse.FileType('r'), help = "file path to counts df")
-    parser.add_argument('-B','--bins_file',metavar='', type = argparse.FileType('r'), help = "file path to bins")
+    number_of_chr = len(list(filter(lambda f: f.endswith('.fna'), os.listdir('.')))) 
+    parser  = argparse.ArgumentParser() 
+    parser.add_argument('-NC','--normal_counts_file', type = argparse.FileType('r'), help = "counts per bin of normal tissue as csv",required=True)
+    parser.add_argument('-CC','--cancer_counts_file', type = argparse.FileType('r'), help = "counts per bin of cancer tissue as csv",required= True)
+    parser.add_argument('-B','--bins_file', type = argparse.FileType('r'), help = "bins file",required=True)
+    parser.add_argument('-cm','--gc_correction_method', type = str, help = "GC-corrction method",default = "Lowess")
+    parser.add_argument('-T','--outlier_threshold', type = int, help = "outlier threshold, default is 10000", default=10000)
+    parser.add_argument('-dn','--desired_number', type = int, help = "desired_number of chromosomes to be normalised",required=True)
+    parser.add_argument('-fr','--fraction', type = float, help = "fhe fraction of the data used when estimating each y-value with Lowess (between 0 and 1)", default=.3)
+
     args = parser.parse_args()
+    desired_number = args.desired_number
+    frac = args.fraction    
     normal_df = comma_sep_data_processing(args.normal_counts_file)
-    normal_df = normal_df.rename({'0': 'normal_counts'}, axis = 1)
+    normal_df = normal_df.set_axis(['bin_ind', 'normal_counts'], axis=1) 
     cancer_df = comma_sep_data_processing(args.cancer_counts_file)
-    cancer_df = cancer_df.rename({'0': 'cancer_counts'}, axis = 1)
+    cancer_df = cancer_df.set_axis(['bin_ind', 'cancer_counts'], axis=1)
     bin_df = space_sep_data_processing(args.bins_file)
     df = pd.merge(bin_df,normal_df,on = 'bin_ind', how = 'inner')
     df = pd.merge(df,cancer_df,on = 'bin_ind', how = 'inner')
-    df = df.drop(df[df.normal_counts > 10000].index)
-    simple_scatter_plot(df['bin_ind'], df['cancer_counts'],'bins','cancer counts','Primary plot with genosomes')
+    threshold = args.outlier_threshold 
+    df = df.drop(df[df.normal_counts > threshold].index)
+    df = df.reset_index(drop=True)
+    #primary plot with geosomes
+    simple_scatter_plot(df,df['bin_ind'], df['cancer_counts'],'bins','cancer counts','Primary plot with genosomes')
     #remove genosomes
-    df = df.drop(df[df.chr_ind == 20].index)
-    df = df.drop(df[df.chr_ind == 21].index)
-    simple_scatter_plot(df['bin_ind'], df['cancer_counts'],'bins','cancer counts','Primary plot without genosomes')
+    while (number_of_chr > desired_number):
+        df = df.drop(df[df.chr_ind == number_of_chr].index)
+        df = df.reset_index(drop=True)
+        number_of_chr = number_of_chr-1
+    simple_scatter_plot(df,df['bin_ind'], df['cancer_counts'],'bins','cancer counts','Primary plot with user given chromosomes')
+    indices = index_of_chr(df, "chr_ind", "bin_start")
+    chr_list = [(SeqIO.read(f"chr{i}.fna", "fasta")).seq  for i in range (1,number_of_chr+1)]
+    df['GC_content'] = countCGall(chr_list,indices,df,number_of_chr)
+    df = df.drop(df[df.GC_content == 0].index)
+    df = df.reset_index(drop=True)
+    simple_scatter_plot(df,df["GC_content"],df["cancer_counts"],"GC-Content","cancer counts","GC-content in cancer counts")
+    if (args.gc_correction_method) == "Lowess":
+        add_column(df,"Lowess_GC_correctd_cancer_counts")
+        add_column(df,"Lowess_GC_correctd_normal_counts")
+        df["Lowess_GC_correctd_cancer_counts"] = gc_correction_withh_lowess(df, "cancer_counts", frac)
+        df["Lowess_GC_correctd_normal_counts"] = gc_correction_withh_lowess(df, "normal_counts", frac)
+        lowess_gc_cancer = sm.nonparametric.lowess(df['Lowess_GC_correctd_cancer_counts'],df['GC_content'], frac=.3)
+        lowess_x_gc_cancer = list(zip(*lowess_gc_cancer))[0]
+        lowess_y_gc_cancer = [element * df['cancer_counts'].median() for element in list(zip(*lowess_gc_cancer))[1]]
+        lowess_gc_normal = sm.nonparametric.lowess(df['Lowess_GC_correctd_normal_counts'],df['GC_content'], frac=.3)
+        lowess_x_gc_normal = list(zip(*lowess_gc_normal))[0]
+        lowess_y_gc_normal = [element * df['normal_counts'].median() for element in list(zip(*lowess_gc_normal))[1]]
+        lowess_cancer = sm.nonparametric.lowess(df['cancer_counts'],df['GC_content'], frac=.3)
+        lowess_x_cancer = list(zip(*lowess_cancer))[0]
+        lowess_y_cancer = list(zip(*lowess_cancer))[1]
+        lowess_normal = sm.nonparametric.lowess(df['normal_counts'],df['GC_content'], frac=.3)
+        lowess_x_normal = list(zip(*lowess_normal))[0]
+        lowess_y_normal = list(zip(*lowess_normal))[1]
+        fig1,axs1=plt.subplots(4,sharex = True)
+        fig1.suptitle('Plot 1')
+        axs1[0].scatter(df['GC_content'], df['cancer_counts'])
+        axs1[0].plot(lowess_x_cancer, lowess_y_cancer, '-b', linewidth = 1)
+        axs1[2].scatter(df['GC_content'],df['normal_counts'])
+        axs1[2].plot(lowess_x_normal, lowess_y_normal, '-b', linewidth = 1)
+        axs1[1].scatter(df['GC_content'], df['Lowess_GC_correctd_cancer_counts']*df['cancer_counts'].median())
+        axs1[1].plot(lowess_x_gc_cancer,lowess_y_gc_cancer, '-b', linewidth = 1)
+        axs1[3].scatter(df['GC_content'], df['Lowess_GC_correctd_normal_counts']*df['normal_counts'].median())
+        axs1[3].plot(lowess_x_gc_normal,lowess_y_gc_normal, '-b', linewidth = 1)
+        axs1[0].set_ylabel('cancer counts',fontsize=7)
+        axs1[1].set_ylabel('corrected GC cancer',fontsize=7)
+        axs1[2].set_ylabel('normal counts', fontsize=7)
+        axs1[3].set_ylabel('corrected GC normal',fontsize=7)
+        axs1[3].set_xlabel('GC_content')
+        plt.savefig('plot1.png', dpi=300, bbox_inches='tight')
+        plt.close()
 
+        fig2, axs2 = plt.subplots(5,sharex=True)
+        fig2.suptitle('Plot 2')
+        plt.rcParams["figure.figsize"] = [30,10]
+        axs2[0].scatter(df['bin_ind'], df['cancer_counts'])
+        axs2[1].scatter(df['bin_ind'], df['Lowess_GC_correctd_cancer_counts']*df['cancer_counts'].median())
+        axs2[2].scatter(df['bin_ind'], df['normal_counts'])
+        axs2[3].scatter(df['bin_ind'], df['Lowess_GC_correctd_normal_counts']*df['normal_counts'].median())
+        axs2[4].scatter(df['bin_ind'], (df['Lowess_GC_correctd_cancer_counts']/df['Lowess_GC_correctd_normal_counts']))   
+        indices = index_of_chr(df,"chr_ind","bin_start")
+        del indices[-1:] 
+        for j in range(5):
+            for i in indices:
+                axs2[j].axvline(x=df.bin_ind[i],ymin=0,ymax=1,c='black')
+        axs2[0].set_ylabel('gc_uncorrected cancer',fontsize=7)
+        axs2[1].set_ylabel('gc_corrected cancer',fontsize=7)
+        axs2[2].set_ylabel('gc_uncorrected normal',fontsize=7)
+        axs2[3].set_ylabel('gc_corrected normal',fontsize=7)
+        axs2[4].set_ylabel('gc_corrected cancer / normal',fontsize=7)
+        plt.xlabel('Chromosoms')
+        plt.savefig('plot2.png', dpi=300, bbox_inches='tight')
+        plt.close()
+        df['GC_corrected_and_control_normalised'] = df["Lowess_GC_correctd_cancer_counts"] / df["Lowess_GC_correctd_normal_counts"]
+        df["GC_corrected_and_control_normalised_diploid"] = (df['GC_corrected_and_control_normalised']*2)/df['GC_corrected_and_control_normalised'].median()
+        df["GC_corrected_and_control_normalised_median"] = df.groupby('chr_ind').GC_corrected_and_control_normalised_diploid.transform('median')
+        df["GC_corrected_and_control_normalised_median"] = df["GC_corrected_and_control_normalised_median"].round(decimals = 0)                           
+        fig3, axs3 = plt.subplots()
+        axs3.scatter(df['bin_ind'], df['GC_corrected_and_control_normalised_diploid']) 
+        fig3.suptitle('Plot 3')
+        indices = index_of_chr(df,"chr_ind","bin_start")
+        del indices[-1:]
+        plt.text(0, .9,'   chr 1',fontsize = 12,verticalalignment ='center') 
+        j = 2
+        for i in indices:
+            axs3.axvline(x=df.bin_ind[i],ymin=0,ymax=1,c='black')
+            axs3.text(df.bin_ind[i], .9, '  chr '+str(j),fontsize = 12,verticalalignment ='center') 
+            j = j+1
+        df[df.chr_ind.isin(list(range(1,20)))].groupby('chr').plot(c ='black',legend= 0,y='GC_corrected_and_control_normalised_median', x='bin_ind', kind='line', ax=axs3,linewidth=5.0)   
+        plt.rcParams["figure.figsize"] = [20.50, 5.50]
+        plt.xlabel('bins',fontsize = 20)
+        plt.ylabel('cancer/control',fontsize = 20)
+        
+        plt.savefig('plot3.png', dpi=300, bbox_inches='tight')
+        plt.close()
 
+    if (args.gc_correction_method) == "median":
+        gc_ncorrection(df,df.GC_content,int((df['GC_content'].min()*10000)),int((df['GC_content'].max()*10000)),5,10000,0.05)
+        gc_ccorrection(df,df.GC_content,int((df['GC_content'].min()*10000)),int((df['GC_content'].max()*10000)),5,10000,0.05)
+        cancer_uncorrected_counts = []
+        cg_uncorrected_cancer = []
+        s =df.sort_values("GC_content",ignore_index=True)
+        for i in range (0,2550,30):
+            cancer_uncorrected_counts.append(s.loc[(i-150):(i+150)].cancer_counts.median())
+            cg_uncorrected_cancer.append(s.loc[(i-150):(i+150)].GC_content.mean())    
+        normal_uncorrected_counts = []
+        cg_uncorrected_normal = []
+        for i in range (0,2550,30):
+            normal_uncorrected_counts.append(s.loc[(i-150):(i+150)].normal_counts.median())
+            cg_uncorrected_normal.append(s.loc[(i-150):(i+150)].GC_content.median())
+        cancer_corrected_counts = []
+        cg_corrected_cancer = []
+        s  = df.sort_values("GC_content",ignore_index=True)
+        for i in range (0,2550,30):
+            cancer_corrected_counts.append(s.loc[(i-150):(i+150)].gc_normalised_cancer_counts.median())
+            cg_corrected_cancer.append(s.loc[(i-150):(i+150)].GC_content.median())
+        normal_corrected_counts = []
+        cg_corrected_normal = []
+        for i in range (0,30,100):
+            normal_corrected_counts.append(s.loc[(i-150):(i+150)].gc_normalised_normal_counts.median())
+            cg_corrected_normal.append(s.loc[(i-150):(i+150)].GC_content.median())
+            
+        fig1,axs1= plt.subplots(4,sharex=True)
+        fig1.suptitle('Plot 1')
+        axs1[0].scatter(df['GC_content'],df['cancer_counts'])
+        axs1[0].plot(cg_uncorrected_cancer, cancer_uncorrected_counts,color="b",linewidth = 1.0)
+        axs1[2].scatter(df['GC_content'],df['normal_counts'])
+        axs1[2].plot(cg_uncorrected_normal, normal_uncorrected_counts,color="b",linewidth=1.0)
+        axs1[1].scatter(df['GC_content'],df['gc_normalised_cancer_counts'])
+        axs1[1].plot(cg_corrected_cancer, cancer_corrected_counts,color = "b",linewidth = 1.0)
+        axs1[3].scatter(df['GC_content'],df['gc_normalised_normal_counts'])
+        axs1[3].plot(cg_corrected_normal,normal_corrected_counts,color="b",linewidth=1.0)
+        axs1[0].set_ylabel('cancer counts',fontsize=7)
+        axs1[1].set_ylabel('corrected GC cancer',fontsize=7)
+        axs1[2].set_ylabel('normal counts', fontsize=7)
+        axs1[3].set_ylabel('corrected GC normal',fontsize=7)
+        axs1[3].set_xlabel('GC_content')
+        plt.savefig('plot1.png', dpi=300, bbox_inches='tight')
+        plt.close()
 
+        fig2, axs2 = plt.subplots(5,sharex=True)
+        fig2.suptitle('Plot 2')
+        plt.rcParams["figure.figsize"] = [30,10]
+        axs2[0].scatter(df['bin_ind'], df['cancer_counts'])
+        axs2[1].scatter(df['bin_ind'], df['gc_normalised_cancer_counts']*df['cancer_counts'].median())
+        axs2[2].scatter(df['bin_ind'], df['normal_counts'])
+        axs2[3].scatter(df['bin_ind'], df['gc_normalised_normal_counts']*df['normal_counts'].median())
+        axs2[4].scatter(df['bin_ind'], (df['gc_normalised_cancer_counts']/df['gc_normalised_normal_counts']))   
+        indices = index_of_chr(df,"chr_ind","bin_start")
+        del indices[-1:] 
+        for j in range(5):
+            for i in indices:
+                axs2[j].axvline(x=df.bin_ind[i],ymin=0,ymax=1,c='black')
+        axs2[0].set_ylabel('gc_uncorrected cancer',fontsize=7)
+        axs2[1].set_ylabel('gc_corrected cancer',fontsize=7)
+        axs2[2].set_ylabel('gc_uncorrected normal',fontsize=7)
+        axs2[3].set_ylabel('gc_corrected normal',fontsize=7)
+        axs2[4].set_ylabel('gc_corrected cancer / normal',fontsize=7)
+        plt.xlabel('Chromosoms')
+        plt.savefig('plot2.png', dpi=300, bbox_inches='tight')
+        plt.close()
 
+        df['GC_corrected_and_control_normalised'] = df["gc_normalised_cancer_counts"] / df["gc_normalised_normal_counts"]
+        df["GC_corrected_and_control_normalised_diploid"] = (df['GC_corrected_and_control_normalised']*2)/df['GC_corrected_and_control_normalised'].median()
+        df["GC_corrected_and_control_normalised_median"] = df.groupby('chr_ind').GC_corrected_and_control_normalised_diploid.transform('median')
+        df["GC_corrected_and_control_normalised_median"] = df["GC_corrected_and_control_normalised_median"].round(decimals = 0)                           
+        fig3, axs3 = plt.subplots()
+        axs3.scatter(df['bin_ind'], df['GC_corrected_and_control_normalised_diploid']) 
+        fig3.suptitle('Plot 3')
+        indices = index_of_chr(df,"chr_ind","bin_start")
+        del indices[-1:]
+        plt.text(0, .9,'   chr 1',fontsize = 12,verticalalignment ='center') 
+        j = 2
+        for i in indices:
+            axs3.axvline(x=df.bin_ind[i],ymin=0,ymax=1,c='black')
+            axs3.text(df.bin_ind[i], .9, '  chr '+str(j),fontsize = 12,verticalalignment ='center') 
+            j = j+1
+        df[df.chr_ind.isin(list(range(1,20)))].groupby('chr').plot(c ='black',legend= 0,y='GC_corrected_and_control_normalised_median', x='bin_ind', kind='line', ax=axs3,linewidth=5.0)   
+        plt.rcParams["figure.figsize"] = [20.50, 5.50]
+        plt.xlabel('bins',fontsize = 20)
+        plt.ylabel('cancer/control',fontsize = 20)
+        plt.tight_layout()
+        plt.savefig('plot3.png', dpi=300, bbox_inches='tight')
+        plt.close()
 
+    
 
 if __name__== "__main__" :
     main()
